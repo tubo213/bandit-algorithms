51f9787ecf16e42632b69e2b50f03a34e0a72229
tubo213 <yuuki.32040808@gmail.com>
2023-07-25 08:43:27 +0900

    update set_seed

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import contextlib
+import os
 import random
 from typing import Optional
 
@@ -6,15 +7,15 @@ import joblib
 import numpy as np
 import torch
 from tqdm.auto import tqdm
-import os
 
 
 def set_seed(seed):
-    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
     np.random.seed(seed)
     random.seed(seed)
     torch.manual_seed(seed)
-
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
 
 
 def concat_context_and_action_context(context, action_context):
